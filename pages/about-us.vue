ca16ddbb40e5de0e4de6c9c44194ae1c691fe369
Cristhian Sanchez <52178427+IDifusal@users.noreply.github.com>
2024-08-13 14:04:53 -0500

    icons

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -38,28 +38,27 @@
                             <div class="section__title-wrapper text-start mb-35 mb-xs-40">
                                 <span
                                     class="section__subtitle section__subtitle-line-height justify-content-start mb-15 mb-xs-5 wow clip-a-z">About 123 Main Mexico</span>
-                                <h2 class="section__title mb-0 title-animation">Delicious Food For Healthy Life</h2>
+                                <h2 class="section__title mb-0 title-animation">Authentic Mexican Flavors, Vibrant Atmosphere</h2>
                             </div>
 
                             <div class="about-2__content-item__wrapper mb-40">
                                 <div class="about-2__content-item mb-35">
                                     <div class="about-2__content-item__media">
-                                        <img src="https://html.rrdevs.net/delish/assets/imgs/about-2/about-1.png" class="img-fluid" alt="not found">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 15 15"><path fill="#CB3A1A" d="M11.433 10.089h2.49v.75h-2.07a4.81 4.81 0 0 1 1.622 2.301l-.71.242a4.01 4.01 0 0 0-1.74-2.197a1.778 1.778 0 0 1-1.035.454l-1.553 1.346H6.52l-1.535-1.362a2.022 2.022 0 0 1-1.093-.44a4.01 4.01 0 0 0-1.743 2.199l-.71-.242a4.815 4.815 0 0 1 1.622-2.301H.99v-.75h2.494l.863-.677l-1.014-1.655a2.274 2.274 0 0 1-.27.029a2.02 2.02 0 0 1-2.052-2.08c0-2.08 1.979-4.16 1.979-4.16l-.737 4.447l1.726-.001L4.99 2.985v2.996a2.094 2.094 0 0 1-.699 1.412l.854 1.394l.084-.066a3.867 3.867 0 0 1 4.478 0l.06.047l.87-1.415a2.118 2.118 0 0 1-.643-1.372V2.985l.983 3.007l1.755.001l-.737-4.447s1.982 2.08 1.982 4.16a2.021 2.021 0 0 1-2.056 2.08a2.313 2.313 0 0 1-.344-.037l-1.01 1.646l.84.659c.013.01.014.024.025.035"/></svg>
                                     </div>
                                     <div class="about-2__content-item__text">
                                         <h6 class="text-uppercase">Quality Food</h6>
-                                        <p class="mb-0">The wise man therefore always holds Indies matters this
-                                            principle information</p>
+                                        <p class="mb-0">Experience the finest, freshest ingredients in every dish at 123 Main Mexico.</p>
                                     </div>
                                 </div>
                                 <div class="about-2__content-item mb-35">
                                     <div class="about-2__content-item__media">
-                                        <img src="https://html.rrdevs.net/delish/assets/imgs/about-2/about-2.png" class="img-fluid" alt="not found">
+                                        
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path fill="#CB3A1A" d="M287.9 76.19v.01h-1.1c-1.9.1-5.4 1.68-9.3 5.36c-3.9 3.69-8 9.05-11.5 14.52C258.9 107 254.2 118 254.2 118l-5.1 12l-9.4-9s-11-10.5-24.4-19.6c-6.7-4.46-14-8.5-20.2-10.59s-10.7-1.98-13.1-.75c-5.2 2.67-9.7 9.27-12.8 19.24c-3.1 10.1-4.6 22.8-5 35.1c-.8 24.7 2.4 47.6 2.4 47.6l2.3 15.9l-14.8-6.3s-9.7-4.2-20.4-6.8c-5.4-1.2-11-2-15-1.9c-4.1.2-5.9 1.2-6.4 1.8c-2.7 3.3-3.9 9.4-2.6 17.9c1.2 8.6 4.8 18.9 9.1 28.5c8.6 19.3 19.8 36 19.8 36l9.8 14.5l-17.6-.5c.1 0-10.1-.3-21.7 1.1c-11.57 1.4-23.95 5.8-26.76 9c-10.59 12.2-14.01 26.1-13.15 41.2c.87 15.2 6.38 31.4 13.83 45.9s16.81 27.3 24.58 35.8c.7.7 1.3 1.4 2 2c16-9.9 25.7-25.6 33.2-46.2c9.2-25.6 14-58 21.3-91.1c7.3-33.2 17.1-67.3 37.7-96.3c20.7-29.1 52.3-52.6 100.7-64c13.7-3.2 26.7-4.7 38.9-4.8q-4.5-6.75-10.5-14.4c-7.1-9.1-15.2-17.93-23.1-24.09c-7.4-5.78-14.5-8.87-19.9-9.02M148.6 118.7c-22 4.2-47.7 28.3-69.23 63.3c-21.89 35.5-39.78 81.1-48.85 123.9c-9.08 42.7-8.81 82.7 2.39 105.7c5.6 11.5 13.22 18.8 24.37 22.2c8.66 2.7 19.98 2.9 34.36-.7c-8.29-9.6-17.18-22.1-24.61-36.6c-8.26-16.1-14.72-34.3-15.79-53c-1.06-18.7 3.64-38.1 17.53-54.1c9.68-11.2 25.08-13.4 38.13-15c3.3-.4 5.6-.5 8.5-.6c-4-6.9-8.7-15.6-13.1-25.3c-4.62-10.6-8.76-22-10.4-33.3c-1.64-11.2-.87-23 6.61-32v-.1c5.19-6.2 12.69-7.9 19.39-8.2c6.8-.2 13.6.9 19.9 2.4c3.4.8 6.5 1.7 9.5 2.6c-.8-9.4-1.5-22.1-1.1-36.1c.3-8.3 1-16.8 2.4-25.1m194.3 22.9c-11.1 0-23.2 1.3-36.2 4.5c-44.9 10.5-71.9 31-90.2 56.8s-27.7 57.4-34.8 89.7c-7.1 32.4-11.8 65.2-22 93.4c-5.4 14.9-12.7 28.7-22.8 40c129.5-22.2 254.5-67 351-112.1c-2.2-14-10.2-56-31.9-96c-13.2-24.3-31.2-46.9-55.2-61c-15.1-8.8-32.5-14.5-53.2-15.2c-1.6-.1-3.1-.1-4.7-.1"/></svg>
                                     </div>
                                     <div class="about-2__content-item__text">
-                                        <h6 class="text-uppercase">Supply Chain Maintain</h6>
-                                        <p class="mb-0">The wise man therefore always holds Indies matters this
-                                            principle information</p>
+                                        <h6 class="text-uppercase">Cozy, central, and inviting</h6>
+                                        <p class="mb-0">Warm and welcoming environment, perfectly situated for a comfortable and enjoyable experience.</p>
                                     </div>
                                 </div>
                             </div>
@@ -79,7 +78,7 @@
 
                     <div class="col-xl-6">
                         <div class="about-2__media wow clip-a-z">
-                            <img src="https://html.rrdevs.net/delish/assets/imgs/about-2/about-food.jpg" class="img-fluid" alt="image not found">
+                            <img src="http://123mainmexico.com/wp-content/uploads/2024/08/gallery-4.jpeg" class="img-fluid" alt="image not found">
                         </div>
                     </div>
                 </div>

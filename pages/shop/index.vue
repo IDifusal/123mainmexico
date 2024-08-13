a3b5a816931c15a5c31533a67a932cae8c57fc7a
Cristhian Sanchez <52178427+IDifusal@users.noreply.github.com>
2024-08-13 08:05:59 -0500

    fix

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -35,9 +35,8 @@
                 <div v-for="product in products" class="col-xl-3 col-md-4 col-sm-6">
                     <div class="food-menu-tab__item mb-24">
                         <nuxt-link :to="`/shop/${product.slug}`"
-                        
                         class="food-menu-tab__item-media">
-                            <img :src="product.images[0]? product.images[0].src : 'https://placehold.co/600x400'" class="img-fluid" alt="image not found">
+                            <img :src="product.images[0]? product.images[0].src : 'http://123mainmexico.com/wp-content/uploads/2024/08/placeholder.png'" class="img-fluid" :alt="product.name">
                         </nuxt-link>
                         <div class="food-menu-tab__item-text">
                             <ul class="rating">

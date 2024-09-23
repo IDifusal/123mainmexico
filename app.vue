59d3ec63b284658db395af39371f60ad9d43936a
Cristhian Sanchez <52178427+IDifusal@users.noreply.github.com>
2024-09-23 16:11:23 -0500

    fix

--- app.vue
+++ app.vue
@@ -14,9 +14,8 @@
               </div>
               <div class="footer-2__subscribe d-flex mt-30 mt-xs-25">
                 <input type="text" placeholder="Email Address">
-                <button type="submit" class="footer-2__subscribe-btn">
-                  <img src="https://html.rrdevs.net/delish/assets/imgs/icon/plane.svg" alt="not found">
-                </button>
+                <button type="submit" class="footer-2__subscribe-btn"><img
+                    src="https://html.rrdevs.net/delish/assets/imgs/icon/plane.svg" alt="not found"></button>
               </div>
             </div>
           </div>
@@ -28,14 +27,94 @@
 </template>
 
 <script setup>
+import { useJsonld } from '#jsonld';
+
+useHead({
+  title: '123 Main Mexico Gastrobar',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/fav-cion.png',
+    },
+    {
+      rel: 'canonical',
+      href: 'http://123mainmexico.com'
+    }
+  ],
+  meta: [
+    {
+      name: 'canonical',
+      content: 'http://123mainmexico.com'
+    }
+    ,
+    { name: 'description', content: '123 Main Mexico offers a delicious variety of coastal Mexican dishes and chef specialties like ribs' }
+  ],
+})
+
 import { onMounted } from 'vue';
 import { useBlogStore } from '@/stores/blogStore';
 import { useProductsStore } from '@/stores/productsStore';
 import { useAsyncData } from 'nuxt/app';
 
 const productsStore = useProductsStore();
+const blogStore = useBlogStore();
 onMounted(() => {
-  blogStore.fetchPosts();
   productsStore.fetchProducts();
+  blogStore.fetchPosts();
+});
+
+const { data: productsData, pending: productsLoading, error: productsError } = await useAsyncData('products', async () => {
+  await productsStore.fetchProducts();
+  return productsStore.products;
+});
+
+// Fetch blog posts using useAsyncData
+const { data: blogData, pending: blogLoading, error: blogError } = await useAsyncData('blog', async () => {
+  await blogStore.fetchPosts();
+  return blogStore.posts;
+});
+
+const restaurant = {
+  name: '123 Main Mexico',
+  description: "We bring the flavors of Mexico's coast to Charlotte in a space that's all about good vibes and great food",
+  url: 'https://www.123mainmexico.com',
+  logo: 'https://www.123mainmexico.com/images/123-MAIN-MEXICO-logo-transparente.png',
+  address: {
+    '@type': 'PostalAddress',
+    streetAddress: '123 Main St',
+    addressLocality: 'Pineville',
+    addressRegion: 'NC',
+    postalCode: '28134',
+    addressCountry: 'US',
+  },
+  telephone: '(704) 889-1236',
+  email: 'reservations@123mainmexico.com',
+  openingHours: [
+    'Su-Th 10:00-22:00',
+    'Fr-Sa 10:00-00:00'
+  ],
+  sameAs: [
+    'https://www.facebook.com/123mainmexico',
+    'https://www.instagram.com/123mainmexico',
+  ],
+};
+
+// Use JSON-LD
+useJsonld({
+  '@context': 'https://schema.org',
+  '@type': 'Restaurant',
+  name: restaurant.name,
+  description: restaurant.description,
+  url: restaurant.url,
+  logo: restaurant.logo,
+  address: restaurant.address,
+  telephone: restaurant.telephone,
+  email: restaurant.email,
+  openingHours: restaurant.openingHours,
+  sameAs: restaurant.sameAs,
 });
 </script>
